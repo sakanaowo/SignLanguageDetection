470c371d37166db599949ca14b85e67045c7ee31
sakanaowo <idiotbugg@gmail.com>
2025-04-23 08:20:18 +0700

    nothing here

--- src/test.py
+++ src/test.py
@@ -3,4 +3,6 @@ from src.data_collection import add_new_action, collect_data
 if __name__ == '__main__':
     """test action"""
     new_action = input('Enter new action: ')
-    if add_new_action(new_action): collect_data([new_action])
+    if add_new_action(new_action):
+        print(new_action)
+        collect_data([new_action])
